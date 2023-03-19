fc8ab9d7d86a7e6d55b7b2570e3d6979df13854c
developer-nieem <naimhossenwork@gmail.com>
2023-03-19 22:40:08 +0600

    46-4 Single Page Application (SPA), Performance benefit

---
+++ singlePageAp.js
@@ -0,0 +1,18 @@
+// benifit
+/* 
+1. fast loading;
+2. Caching capabilities
+3. Improved user experience
+4. Reduced server load:
+5. Easier to develop and maintain
+
+
+*/
+
+// disadvantages
+
+/* 
+1. SEO challenges
+2. Initial load time
+
+*/
