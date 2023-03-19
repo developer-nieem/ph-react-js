2e8f1a1eba7cde70951d61d53171648e64483dab
developer-nieem <naimhossenwork@gmail.com>
2023-03-19 21:15:18 +0600

    Web component, 4 types of component, identify component

---
+++ m-46/components.js
@@ -0,0 +1,8 @@
+//  components divided by 4 
+/* 
+1--- similer in loop diffarent in data  
+2--- Container component
+3---  no pattern, but breckDown for working purpose
+4---- Stand alone component
+
+*/
