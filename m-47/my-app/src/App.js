5ae78c34b2a2c4a7b67cf9615ecde087dc32f1db
developer-nieem <naimhossenwork@gmail.com>
2023-03-20 23:22:53 +0600

    47-5 (advanced) Create multiple components from an array of objects

--- m-47/my-app/src/App.js
+++ m-47/my-app/src/App.js
@@ -14,23 +14,40 @@ const friendStyle = {
 
 }
 const name =  'Nieem Hossen';
-const job = {
-  name: 'nieem',
-  job: 'freelancing'
-}
+const persion = [
+  {name: 'nieem', job: 'freelancing'},
+  {name: 'ismail', job: 'imama'},
+  {name: 'miraz', job: 'politic'},
+  {name: 'saidu', job: 'bekar'},
+  {name: 'Hasan', job: 'Garments'},
+]
 function App() {
+  const names = ['Kalam' , 'salam', 'alam', 'dalam', 'arsalan'] ;
   return (
     <div className="App">
       <header className="App-header">
-       
+       {
+        names.map(name => <li>{name}</li>)
+       }
+         <h2>Below show from array </h2>
+       {
+        names.map(name => <Name name= {name} job={name}></Name>)
+       }
+
+       <h2>Below is for array of map</h2>
+       {
+        persion.map(p => <Name name= {p.name} job ={p.job}></Name>)
+       }
         <div className="main-container">
-          <h2>This is a react server</h2>
+
+          {/* <h2>This is a react server</h2>
           <p style={{color:'blue'}} >My Name : {name}</p>
           <p style={style} > My Name {job.name } im working on {job.job}</p>
-          <p >My Name : {name}</p>
-          <Name name ="nieem" job="freelanching"></Name>
-          <Name name = "hasan" job="garments"></Name>
-          <Name></Name>
+          <p >My Name : {name}</p> */}
+          {/* <Name name ={names[0]} job="freelanching"></Name>
+          <Name name = {names[1]} job="garments"></Name>
+          <Name></Name> */}
+
           <h2>new Components</h2>
           <Friends name="Hasan" like= "Best Friend"></Friends>
           <Friends name =" Tarik" like = "Not Best"></Friends>
@@ -48,7 +65,7 @@ function Name(props) {
   return (
     <div className="persion">
     <h1>{props.name}</h1>
-    <p>Job: {props.job}</p>
+    <p> {props.job}</p>
     </ div>
   )
 }
