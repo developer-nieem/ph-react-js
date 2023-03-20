5e9caf37c81902e898ddda6f30f6cfaf5a169728
developer-nieem <naimhossenwork@gmail.com>
2023-03-20 06:24:04 +0600

    46-6 Explore destructuring and send data to html elements using props

--- m-46/countries.js
+++ m-46/countries.js
@@ -6,7 +6,7 @@ const loadCountries = async () => {
 } 
 
 const displayData = (countrys) =>{
-    console.log(countrys);
+    console.log(countrys[0]);
     const getCountrisHtml = countrys.map(country => displayHtml(country));
     // console.log(getCountrisHtml);
     const getContainer =  document.getElementById('container');
@@ -14,12 +14,38 @@ const displayData = (countrys) =>{
 }
 
 
-const displayHtml = (country) =>{
+// option 3
+const displayHtml = ({name , flags, area}) =>{
+    
+    return `
+        <div class="country">
+        <h2>${name.common}</h2>
+        <p>Area: ${area}</p>
+        <img src="${flags.png}" >
+        </div>
+    `
+}
+
+// option 2 
+/* const displayHtml = (country) =>{
+    const {name , flags} = country
+    return `
+        <div class="country">
+        <h2>${name.common}</h2>
+        <img src="${flags.png}" >
+        </div>
+    `
+}
+ */
+// orginal 
+
+/* const displayHtml = (country) =>{
     return `
         <div class="country">
         <h2>${country.name.common}</h2>
         <img src="${country.flags.png}" >
         </div>
     `
-}
+} */
+
 loadCountries()
