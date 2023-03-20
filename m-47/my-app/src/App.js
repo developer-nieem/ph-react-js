fb800f7e6b6fe6bab33ef10667f7b149b8aa122b
developer-nieem <naimhossenwork@gmail.com>
2023-03-20 22:23:46 +0600

    47-3 Create Component, return HTML from a component

--- m-47/my-app/src/App.js
+++ m-47/my-app/src/App.js
@@ -1,6 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const style = {
+  color: 'blue',
+  backgroundColor: 'white'
+}
+
+const friendStyle = {
+  backgroundColor: 'white',
+  border : '2px solid red',
+  paddign : '20px',
+  margin : '20px'
+
+}
 const name =  'Nieem Hossen';
 const job = {
   name: 'nieem',
@@ -10,28 +22,43 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>JSX</h1>
+       
         <div className="main-container">
           <h2>This is a react server</h2>
           <p style={{color:'blue'}} >My Name : {name}</p>
-          <p > My Name {job.name } im working on {job.job}</p>
+          <p style={style} > My Name {job.name } im working on {job.job}</p>
           <p >My Name : {name}</p>
+          <Name></Name>
+          <Name></Name>
+          <Name></Name>
+          <h2>new Components</h2>
+          <Friends></Friends>
+          <Friends></Friends>
+          <Friends></Friends>
+          <Friends></Friends>
         </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
+}
+
+function Name() {
+  return (
+    <div className="persion">
+    <h1>Nieem</h1>
+    <p>Freelancer</p>
+    </ div>
+  )
+}
+
+const Friends = () => {
+  return (
+    <div style={friendStyle}>
+      <h1>Hasan</h1>
+      <p>Best Friend</p>
+    </div>
+  )
 }
 
 export default App;
