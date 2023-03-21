c48c5606bac6d926b5a8b3da350bee5839c62bfa
developer-nieem <naimhossenwork@gmail.com>
2023-03-21 07:09:33 +0600

    47-7 (advanced) State, component state hook and set state method

--- m-47/new-app/src/App.js
+++ m-47/new-app/src/App.js
@@ -1,31 +1,73 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
-    {name:'laptop', price:50000},
-    {name:'mobile', price:15000},
-    {name:'tab', price:18000},
-    {name:'watch', price:5000}
-  ]
+  
   return (
     <div className="App">
       <header className="App-header">
-        {
-          products.map(product => <Products name={product.name} price={product.price}></Products>)
-        }
-      <Products name ="Laptop" price ='50000'></Products>
+        <Counts></Counts>
+        <ChangeNumber></ChangeNumber>
       </header>
     </div>
   );
 }
 
-const Products = (props) =>{
+const Counts = (props) =>{
+  const [count , setCount] = useState(55);
+  
+  // increass count
+  const countPlus = () =>{
+    const newCount =  count +1;
+    setCount(newCount)
+  }
+
+  // decrase count
+  const decraseCount = () => setCount(count - 1);
+
+  return (
+    <div>
+      <h1>count: {count}</h1>
+      <button onClick={countPlus}>Incress</button>
+      <button onClick={decraseCount}>Decrese</button>
+    </div>
+  )
+}
+
+
+function ChangeNumber(){
+  const [count, setCount] = useState(2);
+  const increassNumber = () => setCount(count+1);
+  const decraseNUmber = () => setCount(count-1);
+  return (
+    <div>
+      <h2>number: {count}</h2>
+      <button onClick={increassNumber}>Increase</button>
+      <button onClick={decraseNUmber}>Decrease</button>
+    </div>
+  )
+}
+export default App;
+
+/* const Products = (props) =>{
   return (
     <div className='products'>
       <h2 style={{color:'red'}}>Product Name : {props.name}</h2>
       <p>Price : {props.price}</p>
     </div>
   )
+} */
+
+/* {
+  products.map(product => <Products name={product.name} price={product.price}></Products>)
 }
-export default App;
+<Products name ="Laptop" price ='50000'></Products> */
+
+
+/* const products = [
+  {name:'laptop', price:50000},
+  {name:'mobile', price:15000},
+  {name:'tab', price:18000},
+  {name:'watch', price:5000}
+] */
