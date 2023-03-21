13be336b271a3cb0cc4a0f2b6054580a2d519bf1
developer-nieem <naimhossenwork@gmail.com>
2023-03-21 07:37:34 +0600

    47-8 (advanced) Load dynamic data, API call useEffect integrate state

--- m-47/new-app/src/App.js
+++ m-47/new-app/src/App.js
@@ -1,18 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   
   return (
     <div className="App">
       <header className="App-header">
-        <Counts></Counts>
-        <ChangeNumber></ChangeNumber>
+        {/* <Counts></Counts>
+        <ChangeNumber></ChangeNumber> */}
+        <ShowUsers></ShowUsers>
       </header>
     </div>
   );
 }
+
+const ShowUsers = () =>{
+const [users, setUsers] = useState([]);
+useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(data => setUsers(data))
+},[])
+
+
+  return (
+    <div>
+      {
+      users.map(user => <User name ={user.name} email={user.email} phone={user.phone}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return (
+    <div style={{border:'2px solid red', margin:'20px'}}>
+      <h2>Name : {props.name}</h2>
+      <p>Email : {props.email}</p>
+      <p>Phone : {props.phone}</p>
+    </div>
+    )
+}
+
+
+
+// for count
 
 const Counts = (props) =>{
   const [count , setCount] = useState(55);
@@ -34,6 +67,7 @@ const Counts = (props) =>{
     </div>
   )
 }
+
 
 
 function ChangeNumber(){
