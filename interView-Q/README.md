3d658bbfcddeb374751575bc79e0df8f0b0a2005
developer-nieem <naimhossenwork@gmail.com>
2023-03-24 20:39:02 +0600

    interveiw

---
+++ interView-Q/README.md
@@ -0,0 +1,15 @@
+1. What is React?
+2. What is the difference between virtual dom and real dom?
+3. What is JSX?
+4. What is the difference between state and props?
+5. What is the difference between class component and
+   functional component?
+
+6. What are react lifecycle methods?
+
+7. What are hooks? Tell us the role of useEffect and
+   useState.
+
+8. What is diff algorithm?
+
+9. What is the difference between attribute and property?
--- m-46/countries.js
+++ m-46/countries.js
@@ -1,32 +1,30 @@
 const loadCountries = async () => {
-    const url = 'https://restcountries.com/v3.1/all';
-    const res = await fetch(url);
-    const data = await res.json();
-    displayData(data)
-} 
+  const url = "https://restcountries.com/v3.1/all";
+  const res = await fetch(url);
+  const data = await res.json();
+  displayData(data);
+};
 
-const displayData = (countrys) =>{
-    console.log(countrys[0]);
-    const getCountrisHtml = countrys.map(country => displayHtml(country));
-    // console.log(getCountrisHtml);
-    const getContainer =  document.getElementById('container');
-    getContainer.innerHTML = getCountrisHtml.join(' ');
-}
-
+const displayData = (countrys) => {
+  console.log(countrys[0]);
+  const getCountrisHtml = countrys.map((country) => displayHtml(country));
+  // console.log(getCountrisHtml);
+  const getContainer = document.getElementById("container");
+  getContainer.innerHTML = getCountrisHtml.join(" ");
+};
 
 // option 3
-const displayHtml = ({name , flags, area}) =>{
-    
-    return `
+const displayHtml = ({ name, flags, area }) => {
+  return `
         <div class="country">
         <h2>${name.common}</h2>
         <p>Area: ${area}</p>
         <img src="${flags.png}" >
         </div>
-    `
-}
+    `;
+};
 
-// option 2 
+// option 2
 /* const displayHtml = (country) =>{
     const {name , flags} = country
     return `
@@ -37,7 +35,7 @@ const displayHtml = ({name , flags, area}) =>{
     `
 }
  */
-// orginal 
+// orginal
 
 /* const displayHtml = (country) =>{
     return `
@@ -48,4 +46,8 @@ const displayHtml = ({name , flags, area}) =>{
     `
 } */
 
-loadCountries()
+loadCountries();
+
+const name = [45, 44, 46, 84, 85];
+
+const num = [458, 897, 6597, 5689];
