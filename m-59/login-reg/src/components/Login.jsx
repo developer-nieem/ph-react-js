598141f8781d0ea9939f41d1899f7d36f5c07623
developer-nieem <naimhossenwork@gmail.com>
2023-04-14 21:50:41 +0600

    some updated

--- m-59/login-reg/src/components/Login.jsx
+++ m-59/login-reg/src/components/Login.jsx
@@ -1,84 +1,69 @@
-import React from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../firebase.config";
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
 
-  const auth =  getAuth(app);
+const [error , setError ]  =useState('');
+const [success , setSuccess] =  useState('')
 
-const formHandler = (event) =>{
-  event.preventDefault();
+const auth =  getAuth(app);
+  const loginHandler = (event) => {
+      event.preventDefault();
 
-      const gmail = event.target.email.value;
-      const pass = event.target.password.value;
-      
+      // 
 
-  createUserWithEmailAndPassword(auth, gmail , pass)
-  .then(result => {
-    const user =  result.user;
-    console.log(user);
-  })
-  .then(error => {
-    console.error(error.message);
-  })
+      const form = event.target;
+      const email =  form.email.value;
+      const pass =  form.password.value;
+      console.log(email, pass);
 
+        setError('');
 
-}
+      if (!/(?=.*[A-Z].*[A-Z])/.test(pass)) {
+        setError('please add two boro hater');
+        return;
+      }else if(!/(?=.*[0-9].*[0-9])/.test(pass)){
+        setError('please add two number');
+        return;
+      }
 
-
-
-
-const emailHandler = (event) =>{
-  // console.log(event.target.value);
-}
-const passwordHandler = (event) => {
-  // console.log(event.target.value);
-}
-
-
+      signInWithEmailAndPassword(auth, email, pass)
+      .then(result => {
+        const loggedUser =  result.user;
+        console.log(loggedUser);
+        setSuccess('Login Done');
+        setError('');
+      })
+      .catch(error => {
+        setError(error.message)
+      })
+  }
   return (
     <div>
-      
+      <h2 className="text-center">Please login </h2>
+     <form onSubmit={loginHandler} className="container mt-3">
+      <div className="form-group mb-3">
+        <label htmlFor="email">Email address</label>
+        <input type="email" className="form-control" id="email" placeholder="Enter email" required />
+      </div>
+      <div className="form-group mb-3">
+        <label htmlFor="password">Password</label>
+        <input type="password" className="form-control" id="password" placeholder="Password"  required />
+      </div>
+      <button type="submit" className="btn btn-primary">Submit</button>
+      <p >
+        {error}
+      </p>
+      <p >
+        {success}
+      </p>
 
-      <form onSubmit={formHandler}>
-        <div className="mb-3">
-          <label for="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input onChange={emailHandler}
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            name="email"
-            required
-          />
-         
-        </div>
-        <div className="mb-3">
-          <label for="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input
-          onBlur={passwordHandler}
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            name="password"
-            required
-          />
-        </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" for="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button type="submit" name="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      <p>If you have already account <Link to='/register'>Register</Link></p>
+    </form>
+
     </div>
   );
 };
--- m-59/login-reg/src/components/Register.jsx
+++ m-59/login-reg/src/components/Register.jsx
@@ -1,31 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification } from "firebase/auth";
+import app from '../firebase.config';
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div>
-             <form>
+  const [error , setError] = useState('');
+  const  [success , setSuccess] =  useState('');
+
+  const auth =  getAuth(app);
+
+const formHandler = (event) =>{
+ 
+ 
+  event.preventDefault();
+
+ 
+  
+      const gmail = event.target.email.value;
+      const pass = event.target.password.value;
+     
+      // setSuccess('');
+      setError('');
+
+      if (!/(?=.*[A-Z].*[A-Z])/.test(pass)) {
+        setError('need boro hater');
+        return;
+      }
+
+  createUserWithEmailAndPassword(auth, gmail , pass)
+  .then(result => {
+    const logedUser =  result.user;
+    console.log(logedUser);
+    sendEmailforVerify(user)
+    event.target.reset();
+    setSuccess('User Created successfully ');
+    setError('')
+    
+  })
+  .catch(error => {
+    console.error(error.message);
+    setError(error.message)
+  })
+
+
+}/*  */
+
+
+const sendEmailforVerify = (user) =>{
+  sendEmailVerification(user)
+  .then((result) => {
+      console.log(result);
+      alert('verify your email')
+  });
+}
+
+
+const emailHandler = (event) =>{
+  // console.log(event.target.value);
+}
+const passwordHandler = (event) => {
+  // console.log(event.target.value);
+}
+
+
+  return (
+    <div>
+      
+
+      <form onSubmit={formHandler}>
         <div className="mb-3">
           <label for="exampleInputEmail1" className="form-label">
             Email address
           </label>
-          <input
+          <input onChange={emailHandler}
             type="email"
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            name="email"
+            required
           />
-          <div id="emailHelp" className="form-text">
-            We'll never share your email with anyone else.
-          </div>
+         
         </div>
         <div className="mb-3">
           <label for="exampleInputPassword1" className="form-label">
             Password
           </label>
           <input
+          onBlur={passwordHandler}
             type="password"
             className="form-control"
             id="exampleInputPassword1"
+            name="password"
+            required
           />
         </div>
         <div className="mb-3 form-check">
@@ -34,11 +101,15 @@ const Register = () => {
             Check me out
           </label>
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" name="submit" className="btn btn-primary">
           Submit
         </button>
+        <p className="text-error">{error}</p>
+        <p className="text-success">{success}</p>
+        <p>If you have already account <Link to='/login'>Login</Link></p>
+        
       </form>
-        </div>
+    </div>
     );
 };
 
